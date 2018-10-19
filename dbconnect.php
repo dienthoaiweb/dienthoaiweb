eed4fe3da61116e92edca6414b570609195b9cec
Dung Ho <htkdung95@gmail.com>
2018-10-19 22:24:53 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect ('localhost', 'root','', 'salomon') or die ("Khong the ket noi den CSDL".mysqli_connect_error());
+$conn = mysqli_connect ('156.67.222.167', 'u883604362_doan','doan@123', 'u883604362_doan') or die ("Khong the ket noi den CSDL".mysqli_connect_error());
 ?>
